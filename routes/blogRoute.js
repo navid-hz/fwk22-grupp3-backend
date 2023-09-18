ed799d6f907752cea5ba75a06796e20f9430e2eb
AD1121 <sinagh30@gmail.com>
2023-09-18 19:30:47 +0200

    errors fixed

--- routes/blogRoute.js
+++ routes/blogRoute.js
@@ -1,15 +1,15 @@
 const express = require("express");
 const router = express.Router();
 
-import {
+const {
   getBlogs,
   getBlogById,
   createBlog,
   updateBlog,
   deleteBlog,
-} from "../controllers/blogController";
+} = require("../controllers/blogController");
 
 router.route("/").get(getBlogs).post(createBlog);
 router.route("/:id").get(getBlogById).put(updateBlog).delete(deleteBlog);
 
-export default router;
+module.exports = router;
