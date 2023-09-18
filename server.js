05c503c18a73a1340a708931a8bc9ec2e35cf9a7
AD1121 <sinagh30@gmail.com>
2023-09-18 19:37:09 +0200

    cors added

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
+const cors = require("cors");
+const port = process.env.PORT || 5000;
 
 dotenv.config();
 
 // Connecting to Mongodb
 connectDB();
 
-const port = process.env.PORT || 5000;
-
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
