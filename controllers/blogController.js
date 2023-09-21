d9adb8807dec2f6dd47ecf62e2ee6d44d1e94893
AD1121 <sinagh30@gmail.com>
2023-09-21 15:18:45 +0200

    #27 UPDATE Request

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -35,48 +35,38 @@ const createBlog = async (req, res) => {
 
 // @desc    Update a blog
 // @route   PUT /blogs/:id
-<<<<<<< HEAD
-
-// Linn
-const updateBlog = async (req, res) => {};
-=======
 const updateBlog = async (req, res) => {
   try {
-    const blogId = req.params.id
-    const { title, content } = req.body
+    const blogId = req.params.id;
+    const { title, content } = req.body;
 
-    const blog = await Blog.findById(blogId)
+    const blog = await Blog.findById(blogId);
 
     if (!blog) {
-      return res.status(404).json({ error: 'Blogpost not found' })
+      return res.status(404).json({ error: "Blogpost not found" });
     }
 
-    blog.title = title
-    blog.content = content
+    blog.title = title;
+    blog.content = content;
 
-    await blog.save()
+    await blog.save();
 
-    res.status(200).json(blog)
+    res.status(200).json(blog);
   } catch (error) {
-    console.error(error)
-    res.status(500).json({ error: 'Server Error'})
+    console.error(error);
+    res.status(500).json({ error: "Server Error" });
   }
 };
->>>>>>> e9eee29f6ff2a96f36a375cd88acfba7c4ed45a0
 
 // @desc    Delete a blog
 // @route   DELETE /blogs/:id
 
-
 const deleteBlog = async (req, res) => {
   try {
     const blogId = req.params.id;
-    
-
-
   } catch (error) {
     console.error(error);
-    res.status(500).json({ error: 'Server error'});
+    res.status(500).json({ error: "Server error" });
   }
 };
 
