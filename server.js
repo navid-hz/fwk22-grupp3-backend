cdb04b2ed4802b70555f6dec4d96c67cedc14920
AD1121 <sinagh30@gmail.com>
2023-09-25 12:47:14 +0200

    test files

--- server.js
+++ server.js
@@ -1,21 +1,10 @@
-const path = require("path");
-const express = require("express");
-const dotenv = require("dotenv").config();
-const connectDB = require("./config/db");
-const port = process.env.PORT || 5000; // .env file content
+const express = require('express')
+const app = express()
 
-const cors = require("cors");
+const PORT = process.env.PORT || 3000
 
-// Connecting to Mongodb
-connectDB();
+app.use('/', (req, res) => {
+  res.status(200).json({"status":"success"})
+})
 
-const app = express();
-
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-
-// Declaring routes
-app.use("/blogs", require("./routes/blogRoute"));
-
-app.listen(port, () => console.log(`Server started on port ${port}`));
+app.listen(PORT, console.log(`Server is listening at port ${PORT}`))
